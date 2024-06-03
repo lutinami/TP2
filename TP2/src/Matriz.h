35d65401bd16bcd48923a9e7c96d4758137ac2c7
lutinami <102843725+lutinami@users.noreply.github.com>
2024-06-02 23:55:13 -0300

    Update Matriz.h

--- TP2/src/Matriz.h
+++ TP2/src/Matriz.h
@@ -29,69 +29,39 @@ public:
 	// -----------------------------------------------------------------------------------------
 
 
-	// Setters ---------------------------------------------------------------------------------
-	void setMatriz(vector<vector<double>> mat);
-	void setMatrizUnidad(int n, int m, double a);
-	void setFilas(int n);						// Cambia la cantidad de
-									// filas a n, sin cambiar
-									// el resto de variables
-	
-	void setColumnas(int m);					// Cambia la cantidad de
-									// columnas a m, sin cambiar
-									// el resto de variables
-	
-	void setCoordenada(int n, int m, double a); 			// Cambia el valor del
-									// elemento en la fila n
-									// y columna m a "a".
-	
-	void setFilaValor(int n, double a);				// Cambia el valor de los
-									// elementos de la fila
-									// n a "a".
-	void setColumnaValor(int m, double a);				// Cambia el valor de los
-									// elementos de la columna
-									// m a "a".
-	void setRango(int n_a, int n_b, int m_c, int m_d, double x);	// Cambia el valor de los
-									// elementos entre la fila
-									// a y b, y la columna
-									// c y d, al valor x
-	// -----------------------------------------------------------------------------------------
+	// Setters -------------------------------------------------------------------------------------------------------------------------
+	void setMatriz(vector<vector<double>> mat);			// Cambia el valor de matriz con un vector 2D
+	void setMatrizUnidad(int n, int m, double a);			// Cambia el valor de matriz con los nuevos tamanos
+	void setFilas(int n);						// Cambia el valor cantidad de filas
+	void setColumnas(int m);					// Cambia el valor cantidad de columnas
+	void setCoordenada(int n, int m, double a); 			// Cambia el valor del elemento en (n;m)
+	void setFilaValor(int n, double a);				// Cambia el valor de los elementos en la fila n
+	void setColumnaValor(int m, double a);				// Cambia el valor de los elementos en la col m
+	void setRango(int n_a, int n_b, int m_c, int m_d, double x);	// Cambia el valor de los elementos entre rangos de columnas y filas
+	// ---------------------------------------------------------------------------------------------------------------------------------
 
 
-	// Getters --------------------------------------------------------
-	vector<vector<double>> getMatriz(); 	// devuelve la matriz de un
-						// objeto Matriz
-	int getFilas(); 			// devuelve cantidad de
-						// filas
-	int getColumnas(); 			// devuelve cantidad de
-						// columnas
-	// ----------------------------------------------------------------
+	// Getters -------------------------------------------------------------
+	vector<vector<double>> getMatriz(); 	// devuelve la matriz
+	int getFilas(); 			// devuelve cantidad de filas
+	int getColumnas(); 			// devuelve cantidad de columnas
+	// ---------------------------------------------------------------------
 
 	// Funciones miembro --------------------------------------------------------------
 	Matriz& ProductoPorEscalar(double esc);	// producto escalar de la matriz
-	virtual int numel(); 				// devuelve el numero total del elementos
-						// de la matriz
-	double cumsum();			// devuelve la suma de todos los elementos
-						// de la matriz
+	virtual int numel(); 			// devuelve la cantidad de elementos de la matriz
+	double cumsum();			// devuelve la suma de todos los elementos de la matriz
 	double max();				// devuelve el maximo elemento de la matriz
 	double min();				// devuelve el minimo elemento de la matriz
 	// --------------------------------------------------------------------------------
 
-	// Sobrecarga de operadores --------------------------------------------------
-	friend ostream& operator<<(ostream& os, Matriz& mat);	// permite imprimir en
-								// pantalla la matriz
-								// del objeto
-	Matriz operator+(const Matriz& q)const;			// permite la suma de
-								// matrices con el
-								// operador +
-	Matriz operator-(const Matriz& q)const;			// permite la resta de
-								// matrices con el
-								// operador -
-	Matriz operator!()const;				// permite obtener la
-								// matriz traspuesta
-	Matriz operator*(const Matriz& q)const;			// permite el producto
-								// matricial con el
-								// operador *
-	// ---------------------------------------------------------------------------
+	// Sobrecarga de operadores -------------------------------------------------------------------
+	friend ostream& operator<<(ostream& os, Matriz& mat);	// imprime en pantalla la matriz con <<
+	Matriz operator+(const Matriz& q)const;			// suma matrices con +
+	Matriz operator-(const Matriz& q)const;			// resta matrices con -
+	Matriz operator!()const;				// devuelve la matriz traspuesta con !
+	Matriz operator*(const Matriz& q)const;			// devuelve el producto matricial con *
+	// --------------------------------------------------------------------------------------------
 
 	// Destructores --------------------------------------------------------------
 	virtual ~Matriz();
