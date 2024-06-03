d76c97412e81733eab324e060fbfb116c024e1a7
lutinami <102843725+lutinami@users.noreply.github.com>
2024-06-03 09:36:20 -0300

    Update MatrizIdentidad.cpp

--- TP2/src/MatrizIdentidad.cpp
+++ TP2/src/MatrizIdentidad.cpp
@@ -7,32 +7,36 @@
 
 #include "MatrizIdentidad.h"
 
+// Constructores ------------------------------------------------------------------
 MatrizIdentidad::MatrizIdentidad() {
-	MatrizCuadrada ident = MatrizCuadrada();
-	ident = ident*(~ident);
-	this->matriz = ident.getMatriz();
-	this->size_n = ident.getSize_n();
-	this->columnas = ident.getColumnas();
-	this->filas = ident.getFilas();
+	MatrizCuadrada ident = MatrizCuadrada();	// Crea la matriz identidad
+	ident = ident*(~ident);				// como producto matricial
+	this->matriz = ident.getMatriz();		// entre una matriz 4x4
+	this->size_n = ident.getSize_n();		// y su inversa
+	this->columnas = ident.getColumnas();		//
+	this->filas = ident.getFilas();			//
 }
-
+// ---------------------------------------------------------------------------------
 MatrizIdentidad::MatrizIdentidad(MatrizCuadrada M) {
-	M = M*(~M);
-	this->matriz = M.getMatriz();
-	this->size_n = M.getSize_n();
-	this->columnas = M.getColumnas();
-	this->filas = M.getFilas();
+	M = M*(~M);				// Crea la matriz identidad como
+	this->matriz = M.getMatriz();		// producto matricial entre la
+	this->size_n = M.getSize_n();		// matriz cuadrada M y su inversa
+	this->columnas = M.getColumnas();	//
+	this->filas = M.getFilas();		//
 }
-
+// ---------------------------------------------------------------------------------
 MatrizIdentidad::MatrizIdentidad(int n) {
-	MatrizCuadrada ident = MatrizCuadrada(n);
-	ident = ident*(~ident);
-	this->matriz = ident.getMatriz();
-	this->size_n = ident.getSize_n();
-	this->columnas = ident.getColumnas();
-	this->filas = ident.getFilas();
+	MatrizCuadrada ident = MatrizCuadrada(n);	// Crea la matriz identidad
+	ident = ident*(~ident);				// como producto matricial
+	this->matriz = ident.getMatriz();		// entre una matriz nxn y
+	this->size_n = ident.getSize_n();		// su inversa
+	this->columnas = ident.getColumnas();		//
+	this->filas = ident.getFilas();			//
 }
+// ---------------------------------------------------------------------------------
 
+// Destructores ------------------------------
 MatrizIdentidad::~MatrizIdentidad() {
 	// TODO Auto-generated destructor stub
 }
+// -------------------------------------------
