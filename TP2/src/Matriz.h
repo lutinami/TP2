8aeb788da93c1d5e6e1f7a7e96a64ee73495e93b
lutinami <102843725+lutinami@users.noreply.github.com>
2024-06-04 18:26:37 -0300

    Update Matriz.h

--- TP2/src/Matriz.h
+++ TP2/src/Matriz.h
@@ -22,18 +22,18 @@ protected:
 public:
 	// Constructores ---------------------------------------------------------------------------
 	Matriz();				// Matriz por defecto
-	Matriz(int n, int m);			// Matriz de n filas y m columnas
+	Matriz(int n, int m);			// Matriz aleatoria de n filas y m columnas
 	Matriz(int n, int m, double a);		// Matriz de valor "a" de n filas y m columnas
 	Matriz(vector<vector<double>> matriz);	// Matriz definida por sus elementos
 	Matriz(const Matriz& M);		// Matriz copia
 	// -----------------------------------------------------------------------------------------
 
 
-	// Setters -------------------------------------------------------------------------------------------------------------------------
-	void setMatriz(vector<vector<double>> mat);			// Cambia el valor de matriz con un vector 2D
-	void setMatrizUnidad(int n, int m, double a);			// Cambia el valor de matriz con los nuevos tamanos
-	void setMatrizAleatoria(int n, int m);
-	// ---------------------------------------------------------------------------------------------------------------------------------
+	// Setters ---------------------------------------------------------------------------------------------------------
+	void setMatriz(vector<vector<double>> mat);	// Cambia la matriz con un vector 2D
+	void setMatrizUnidad(int n, int m, double a);	// Cambia la matriz por una matriz "a" de n filas y m columnas
+	void setMatrizAleatoria(int n, int m);		// Cambia la matriz por una matriz aleatoria de n filas y m columnas 
+	// -----------------------------------------------------------------------------------------------------------------
 
 
 	// Getters -------------------------------------------------------------
@@ -42,13 +42,13 @@ public:
 	int getColumnas(); 			// devuelve cantidad de columnas
 	// ---------------------------------------------------------------------
 
-	// Funciones miembro --------------------------------------------------------------
+	// Funciones miembro --------------------------------------------------------------------------
 	Matriz& ProductoPorEscalar(double esc);	// producto escalar de la matriz
 	virtual int numel(); 			// devuelve la cantidad de elementos de la matriz
 	double cumsum();			// devuelve la suma de todos los elementos de la matriz
 	double max();				// devuelve el maximo elemento de la matriz
 	double min();				// devuelve el minimo elemento de la matriz
-	// --------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------
 
 	// Sobrecarga de operadores -------------------------------------------------------------------
 	friend ostream& operator<<(ostream& os, Matriz& mat);	// imprime en pantalla la matriz con <<
